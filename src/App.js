903677f451d8bc351c8accc861e704e06f36b3d2
jadhav-kunal <jadhavkunal1999@gmail.com>
2023-07-24 15:14:38 +0530

    Added dummy data and some styling

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import './App.css';
 import Lane from './components/Lane/Lane';
 import TaskForm from './components/TaskForm/TaskForm';
-import generateDummyData from './data';
+import generateDummyData from './utils/data';
 
 function App() {
   const [todoList, setTodoList] = useState([]);
